edf4fa00ee654f4e1a7b67f51b15f8d15ff12185
Prashant Rajendra Londhe <90589964+Prashantlondhe123@users.noreply.github.com>
2023-04-06 14:15:07 +0530

    Create CTE .sql

---
+++ CTE .sql
@@ -0,0 +1,1 @@
+# CTE : A common table expression, or CTE, is a temporary named result set created from a simple SELECT statement that can be used in a subsequent SELECT statement. Each SQL CTE is like a named query, whose result is stored in a virtual table (a CTE) to be referenced later in the main query
